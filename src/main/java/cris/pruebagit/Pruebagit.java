ee35b5781f975f19f0c8f6f5f18054814014b8aa
Diurno <Diurno@C06PC04.informatica.comercio>
2023-11-30 10:48:40 +0100

    edghdgf

---
+++ src/main/java/cris/pruebagit/Pruebagit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cris.pruebagit;
+
+/**
+ *
+ * @author Diurno
+ */
+public class Pruebagit {
+
+    public static void main(String[] args) {
+        System.out.println("Lo que sea!");
+    }
+}
